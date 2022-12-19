8ecd34db14f704bca206895207e9b3866d68a637
alekslesik <alekslesik@gmail.com>
2022-12-19 12:47:22 +0300

    Creating the TokenModel and Validation Checks

--- internal/data/models.go
+++ internal/data/models.go
@@ -7,7 +7,6 @@ import (
 
 // Define a custom ErrRecordNotFound error. We'll return this from our Get() method when
 // looking up a movie that doesn't exist in our database.
-
 var (
 	ErrRecordNotFound = errors.New("record not found")
 	ErrEditConflict = errors.New("edit conflict")
@@ -17,6 +16,7 @@ var (
 type Models struct {
 	Movies MovieModel
 	Users UserModel
+	Tokens TokenModel
 }
 
 // For ease of use, we also add a New() method which returns a Models struct containing
@@ -25,6 +25,7 @@ func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
 		Users: UserModel{DB: db},
+		Tokens: TokenModel{DB: db},
 	}
 }
 
