15fe43d342f906a0db47b51716dcd54d07e58835
alekslesik <alekslesik@gmail.com>
2022-12-29 18:16:41 +0300

    Requiring User Activation

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -93,3 +93,15 @@ func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter
 	message := "invalid or missing authentication token"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
 }
+
+// Send a 401 Unauthorized if user is not authorized
+func (app*application) authenticationRequiredResponse(w http.ResponseWriter, r *http.Request) {
+	message := "you must be authenticated to access this resource"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
+
+// Send a 403 Forbidden if user is not activated
+func (app*application) inactiveAccountResponse(w http.ResponseWriter, r *http.Request) {
+	message := "your user account must be activated to access this resource"
+	app.errorResponse(w, r, http.StatusForbidden, message)
+}
--- cmd/api/routers.go
+++ cmd/api/routers.go
@@ -24,16 +24,16 @@ func (app *application) routes() http.Handler {
 	// endpoints using the HandlerFunc() method. Note that http.MethodGet and
 	// http.MethodPost are constants which equate to the strings "GET" and "POST" respectively
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies",  app.listMoviesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.requireActivatedUser(app.showMovieHandler))
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 	//Register a new user; POST {"name":"", "email":"", "password":""}
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	// Activate a specific user; PUT {"token":""}
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
-	// Generate a new authentication token
+	// Generate a new authentication token; POST {"email": "", "password": ""}'
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
 	// Wrap the router with the panic recovery middleware.
